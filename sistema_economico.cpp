678e3c85e45d574e702fa6b54cc2d0619785681d
Samuel <sgpozadab@unal.edu.co>
2021-04-25 23:01:23 -0500

    vector de distribución añadido

--- sistema_economico.cpp
+++ sistema_economico.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <cstdlib>
 
+double lambda = 0.5; //constant saving propensity
+const int N = 250; //number of agents
+const int W = 13; //number of states
 double T = 10.0; //average money per agent
-double lambda = 0.5; // constant saving propensity
-const int N = 25; //number of agents
+//M = T*N is the total money 
 
-double agents[N];
-double random_number(double nmax);
+double agents[N]; //money per agent
+int states[W] = {}; //occupation number per state
 double random_agent();
-double normal_rnum();
-double minimum(double a, double b);
-void initialize_agents(double ag[N], double T);
-void show_agents(double ag[N]);
-void interaction(double ag[N], int NSTEPS);
+double normal_rnum(); //epsilon
+void initialize_agents(double ag[], double T);
+void show_agents(double ag[]);
+void show_states(int st[]);
+void interaction(double ag[N], int NSTEPS); //exchange rule between two random agents
+void counting(double ag[N], int st[W]); //evaluates the occupation numbers
 
 int main(){
 	
-	srand(45); //rand function seed
+	srand(0); //rand function seed
 	
 	/*for(int k=0; k<20; k++){
 		std::cout << random_agent() << std::endl;
@@ -24,34 +27,34 @@ int main(){
 	
 	initialize_agents(agents, T);
 	interaction(agents, 1000000);
-	show_agents(agents);
+	counting(agents, states);
+	show_states(states);
  
 	return 0;
 }
 
-void initialize_agents(double ag[N], double T){
+void initialize_agents(double ag[], double T){
 	
-	for(int i=0; i<N; i++){
-		ag[i] = T;
+	for(int k=0; k<N; k++){
+		ag[k] = T;
 	}
 }
 
-void show_agents(double ag[N]){
+void show_agents(double ag[]){
 	
-	for(int i=0; i<N; i++){
-		std::cout<< ag[i] << "\n";
+	for(int k=0; k<N; k++){
+		std::cout<< ag[k] << "\n";
+	}
+}
+
+void show_states(int st[]){
+	
+	for(int k=0; k<W; k++){
+		std::cout<< st[k] << "\n";
 	}
 }
 
 //rand function generates random numbers between 0 and RAND_MAX=32767
-//rand()/RAND_MAX generates numbers between 0 and 1
-double random_number(double nmax){
-	
-	double rnumber = 2*nmax*rand()/RAND_MAX-nmax;
-	//generates numbers between -nmax and +nmax
-	return rnumber;
-}
-
 double random_agent(){
 	
 	int ragent = rand()%N;
@@ -59,47 +62,41 @@ double random_agent(){
 	return ragent;
 }
 
+//rand()/RAND_MAX generates numbers between 0 and 1
 double normal_rnum(){
 	
 	double repsilon = 1.0*rand()/RAND_MAX;
 	return repsilon;
 }
 
-double minimum(double a, double b){
-	
-	if(a<b) return a;
-	else return b;
-}
-
-/*void interaction(double ag[N]){
-	
-	double Delta_m = 0;
-	
-	for(int i=0; i<N; i++){
-		for(int j=0; j<N; j++){
-			double min = minimum(agents[i], agents[j]);
-			Delta_m = random_number(min);
-			ag[i] += Delta_m;
-			ag[j] -= Delta_m;
-			//std::cout << min << "\t" << Delta_m << std::endl;
-		}
-	}
-}*/
-
 void interaction(double ag[N], int NSTEPS){
 	
 	for(int k=0; k<NSTEPS; k++){
 		double Delta_m = 0;
 		double epsilon = normal_rnum();
-		int a = random_agent();
-		int b = random_agent();
+		int i = random_agent();
+		int j = random_agent();
 		
-		if(a!=b){
-			Delta_m = (1.0-lambda)*(epsilon*ag[b]-(1.0-epsilon)*ag[a]);
-			ag[a] += Delta_m;
-			ag[b] -= Delta_m;
+		if(i!=j){
+			Delta_m = (1.0-lambda)*(epsilon*ag[j]-(1.0-epsilon)*ag[i]);
+			ag[i] += Delta_m;
+			ag[j] -= Delta_m;
 		}
 		//std::cout << a << "\t" << b << "\t" << epsilon << "\t" << Delta_m << std::endl;
 	}
 }
 
+//ag[k]/T is the normalized money of agent-k
+void counting(double ag[N], int st[W]){
+	
+	for(int k=0; k<N; k++){
+		double x = 3.0*ag[k]/T;
+		
+		if(x<12){
+			st[(int)x] += 1;
+		}
+		else{
+			st[12] += 1;
+		}
+	}
+}
